79a75700bd2f8d2c49842a3d38cf4ad04541a6bd
Kolawole Ezekiel <doneazy911@gmail.com>
2024-04-21 19:41:01 +0100

    add google line chart to display data on a graph

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,4 +1,5 @@
 require 'waqi'
+require 'ostruct'
 
 class FeedsController < ApplicationController
     before_action :set_client
@@ -6,7 +7,24 @@ class FeedsController < ApplicationController
     def index
         @city = request.query_parameters['city']
         if !@city.blank?
-            @feeds = @waqi_client.get_city_feed(@city)
+            response = @waqi_client.get_city_feed(@city)
+
+            if response["status"] == "ok"
+                @response = response["data"]
+                @city_info = response["data"]["city"]
+
+                @iaqi = response["data"]["iaqi"].map do |key, value|
+                      { "name" => key.to_s, "value" => value["v"] } 
+                end
+
+                @forecast = response["data"]["forecast"]["daily"]
+
+                @daily_forecast = format_forecast_data(@forecast)
+
+            elsif response["status"] == "error"
+                @isError = true
+                @response = response
+            end
         end
     end
 
@@ -14,5 +32,26 @@ class FeedsController < ApplicationController
     
     def set_client
         @waqi_client = Waqi::Client::new(api_key: Rails.application.credentials[:air_quality_data_token])
+    end
+
+    def format_forecast_data(air_quality_data)
+        # Extract days from "o3", "pm10", and "pm25" arrays
+        o3_days = air_quality_data["o3"].map { |data| data["day"] } || []
+        pm10_days = air_quality_data["pm10"].map { |data| data["day"] } || []
+        pm25_days = air_quality_data["pm25"].map { |data| data["day"] } || []
+      
+        # Find intersection of the three arrays
+        identical_days = o3_days & pm10_days & pm25_days
+        formatted_parameter_data = []
+      
+          # Extract required values and push into new array
+        identical_days.each do |date|
+            o3_index = air_quality_data["o3"].find_index { |entry| entry["day"] == date }
+            pm10_index = air_quality_data["pm10"].find_index { |entry| entry["day"] == date }
+            pm25_index = air_quality_data["pm25"].find_index { |entry| entry["day"] == date }
+
+            formatted_parameter_data << [date, air_quality_data["o3"][o3_index]["avg"], air_quality_data["pm10"][pm10_index]["avg"], air_quality_data["pm25"][pm25_index]["avg"]]
+        end
+        formatted_parameter_data
     end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,28 @@
 module ApplicationHelper
+    def determineLabel(name)
+        case name
+        when "co"
+            { "label" => "Carbon monoxide", "unit" => "ppm" }
+        when "no2"
+            { "label" => "Nitrogen dioxide", "unit" => "ppb" }
+        when "pm10"
+            { "label" => "PM10", "unit" => "µg/m³" }
+        when "pm25"
+            { "label" => "PM25", "unit" => "µg/m³" }
+        when "o3"
+            { "label" => "Ozone", "unit" => "ppb" }
+        when "w"
+            { "label" => "Wind Speed", "unit" => "m/s" }
+        when "t"
+            { "label" => "Temperature", "unit" => "°C" }
+        when "p"
+            { "label" => "Air pressure", "unit" => "mb" }
+        when "so2"
+            { "label" => "Sulfur dioxide", "unit" => "ppb" }
+        when "h"
+            { "label" => "Humidity", "unit" => "%"} 
+        when "dew"
+            { "label" => "Dew", "unit" => "°C"}
+        end
+    end
 end
